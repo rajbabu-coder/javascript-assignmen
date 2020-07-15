9ba470129b75fc7a74c4cc7782b60c704737eb3c
rajbabu-coder <68226208+rajbabu-coder@users.noreply.github.com>
2020-07-15 14:08:57 +0530

    Add files via upload

---
+++ console.js
@@ -0,0 +1,26 @@
+console.count("Rajbabu");
+console.count("Rajbabu");
+//"This method is used to count the operations done in console log
+
+console.assert(document.getElementById("sample"), "There was an error in ID name of sample");
+//"This method is used to throw an error if the statement or condition gets false."
+
+console.log("Hi! This is new one");
+function myFunction() {
+    console.clear();
+}
+//"This method is used to clear the console and writes the user message in console"
+
+console.error("You made a mistake");
+//"This method is used to throw some error in console"
+
+console.log("This is a log");
+console.group();
+console.log("This time in a group");
+//"The method indicates the start of a message group. After giving the method all messages will from now on be written inside this group."
+
+console.info("Hello world");
+//"This method just simply throws the user message in the console"
+
+console.table(["Apple","Strawberry","Mango"]);
+//"This method is used to write the table view in the console.
